1fdb0745f73455a587710ef21800bd4b72a05275
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-11 13:49:17 -0800

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python script to export
+data in the CSV format
+"""
+import csv
+import requests
+from sys import argv
+
+
+user_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+req = requests.get(user_url)
+user_data = req.json()
+
+todo_url = ("https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(argv[1]))
+req = requests.get(todo_url)
+todo_data = req.json()
+
+csvname = "{}.csv".format(argv[1])
+with open(csvname, 'w') as csvfile:
+    fieldnames = ["userId", "name", "completed", "title"]
+    writer = csv.DictWriter(
+        csvfile, fieldnames=fieldnames, extrasaction='ignore')
+    for task in todo_data:
+        task["name"] = user_data["name"]
+        writer.writerow(task)
