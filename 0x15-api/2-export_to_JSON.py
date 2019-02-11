b3ad288b85464b30de0a2a971ea0d9d6ee3d041b
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-11 14:04:44 -0800

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,7 +3,7 @@
 Using what you did in the task #0, extend your Python script to export
 data in the CSV format
 """
-import csv
+import json
 import requests
 from sys import argv
 
@@ -15,13 +15,14 @@ user_data = req.json()
 todo_url = ("https://jsonplaceholder.typicode.com/todos?userId={}"
             .format(argv[1]))
 req = requests.get(todo_url)
+
 todo_data = req.json()
 
-csvname = "{}.csv".format(argv[1])
-with open(csvname, 'w') as csvfile:
-    fieldnames = ["userId", "name", "completed", "title"]
-    writer = csv.DictWriter(
-        csvfile, fieldnames=fieldnames, extrasaction='ignore')
-    for task in todo_data:
-        task["name"] = user_data["username"]
-        writer.writerow(task)
+
+todo_data = {argv[1]: [
+    dict(item, **{'username': user_data["username"]}) for item in todo_data
+]}
+
+jsonname = "{}.json".format(argv[1])
+with open(jsonname, 'w') as outfile:
+    json.dump(todo_data, outfile)
