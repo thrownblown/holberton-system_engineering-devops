5bddd3979e4d67a1387496c37788491d059fa4e1
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-11 14:13:10 -0800

    dict of dicts

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python script to export
+data in the CSV format
+"""
+import json
+import requests
+
+final_data = {}
+user_url = "https://jsonplaceholder.typicode.com/users/"
+req = requests.get(user_url)
+user_data = req.json()
+for user in user_data:
+    todo_url = ("https://jsonplaceholder.typicode.com/todos?userId={}"
+                .format(user["id"]))
+    req = requests.get(todo_url)
+
+    todo_data = req.json()
+
+    final_data[user["id"]] = [
+        dict(item, **{'username': user["username"]}) for item in todo_data
+    ]
+
+jsonname = "todo_all_employees.json"
+with open(jsonname, 'w') as outfile:
+    json.dump(final_data, outfile)
