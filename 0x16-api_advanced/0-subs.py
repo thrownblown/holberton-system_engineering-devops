274f3d222a9c24ccba7165ef80ba5e1485d5c493
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-13 12:51:07 -0800

    docs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """ returns the number of subscribers  """
     url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'holberbot'})
-    data = requests.get(url, headers=headers).json()
+    data = requests.get(url, headers=headers, allow_redirects=True).json()
     if 'subscribers' in data['data']:
         return data['data']['subscribers']
     return 0
