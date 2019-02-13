606ecc2c787b0d2cddd2afc50817fafc94aea312
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-13 12:48:02 -0800

    init

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers  """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'holberbot'})
+    data = requests.get(url, headers=headers).json()
+    if 'subscribers' in data['data']:
+        return data['data']['subscribers']
+    return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,5 @@
+total 12
+-rwxrw-r-- 1 thrownblown thrownblown  299 Feb 13 12:41 0-main.py
+-rwxrw-r-- 1 thrownblown thrownblown  452 Feb 13 12:47 0-subs.py
+drwxr-xr-x 2 thrownblown thrownblown 4096 Feb 13 12:47 __pycache__
+-rw-r--r-- 1 thrownblown thrownblown    0 Feb 13 12:47 README.md
